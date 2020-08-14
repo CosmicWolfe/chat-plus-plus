d5539e96849b8caf44ef9fd07b409170f9db32a6
Jamie Sebastian <jamiesebastianjo@gmail.com>
2020-08-14 19:27:31 -0400

    add high level ui structure

--- src/app/chats/chats.component.ts
+++ src/app/chats/chats.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon'
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-chats',
