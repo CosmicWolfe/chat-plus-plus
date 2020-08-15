7da9a241aaf5e296a371ca55bf4e822df998a56a
MJA1017 <michael.joseph1017@gmail.com>
2020-08-15 13:33:34 +0700

    profile new page

--- src/app/chats/chats.component.ts
+++ src/app/chats/chats.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon'
-import { MatDividerModule } from '@angular/material/divider';
-import { FormsModule } from '@angular/forms';
+// import { MatIconModule } from '@angular/material/icon'
+// import { MatDividerModule } from '@angular/material/divider';
+// import { FormsModule } from '@angular/forms';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-chats',
@@ -11,12 +12,15 @@ import { FormsModule } from '@angular/forms';
 export class ChatsComponent implements OnInit {
   chats = ['1', '2', '3'];
   @Output('changeChatEvent') changeChatEvent = new EventEmitter();
-  constructor() { }
 
+  constructor(private router : Router){}
   ngOnInit(): void {
   }
 
   handleChangeChat(chatId: string)  {
     this.changeChatEvent.emit(chatId);
+  }
+  goToPage(pageName:string):void{
+    this.router.navigate([`${pageName}`]);
   }
 }
