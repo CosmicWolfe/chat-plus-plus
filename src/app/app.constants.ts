e4c3a6fa84abebc4ad708714d362b27848cc6c8d
Jamie Sebastian <jamiesebastianjo@gmail.com>
2020-08-15 14:04:12 -0400

    constants

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,1 +1,10 @@
-declare var SEC;
+export class KeyCodes {
+    public static KEY_LEFT = 37
+    public static KEY_UP = 38;
+    public static KEY_RIGHT = 39;
+    public static KEY_DOWN = 40;
+    public static KEY_ENTER = 13;
+  
+    constructor() {
+    }
+}
