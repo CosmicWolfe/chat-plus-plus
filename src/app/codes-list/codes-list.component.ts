a94fbb829ff3e1ca3f1d2372ca7dc14a2549b870
jt3698 <45274649+jt3698@users.noreply.github.com>
2020-08-15 22:22:25 -0400

    Code submitting

--- src/app/codes-list/codes-list.component.ts
+++ src/app/codes-list/codes-list.component.ts
@@ -24,15 +24,11 @@ export class CodesListComponent implements OnInit {
   ngAfterViewInit(): void {
     //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
     //Add 'implements AfterViewInit' to the class.
-    this.codeService.refreshCodes();
     this.SecWidget = [null, null, null];
+    this.codeService.refreshCodes(this.SecWidget);
     
-      this.SecWidget[0] = this.codeService.SEC.widget("A");
-      this.SecWidget[0].events.subscribe('beforeSendSubmission', this.submitA.bind(this));
-      this.SecWidget[1] = this.codeService.SEC.widget("B");
-      this.SecWidget[1].events.subscribe('beforeSendSubmission', this.submitB.bind(this));
-      this.SecWidget[2] = this.codeService.SEC.widget("C");
-      this.SecWidget[2].events.subscribe('beforeSendSubmission', this.submitC.bind(this));
+
+      
   }
 
   public expandToggle() {
@@ -40,59 +36,12 @@ export class CodesListComponent implements OnInit {
     this.expanded = !this.expanded;
   }
 
-  public submitA = (data) => {
-    console.log("SUBMITTED");
-    this.codeService.submitCode(this.chatID,
-                                "A",
-                                data.submissionSource,
-                                data.submissionLanguage,
-                                data.submissionInput);
-    console.log("SUBMITTED");
-    return true;
-  }
-  public submitB = (data) => {
-    this.codeService.submitCode(this.chatID,
-                                "B",
-                                data.submissionSource,
-                                data.submissionLanguage,
-                                data.submissionInput);
-
-    console.log("SUBMITTED");
-    return true;
-  }
-  public submitC = (data) => {
-    this.codeService.submitCode(this.chatID,
-                                "C",
-                                data.submissionSource,
-                                data.submissionLanguage,
-                                data.submissionInput);
-
-    console.log("SUBMITTED");
-    return true;
-  }
+  
 
 
   async ngOnChanges(changes : SimpleChanges) {
     if (changes.chatID && this.chatID) {
-      let widgets = ["A", "B", "C"];
-      let ctr = 0;
-      for (let index of widgets) {
-        var code = this.codeService.getCode(this.chatID, index);
-
-        let sourceCode : string;
-        let compiler : number;
-        let submissionInput : string;
-        let snapshot = await code.child('sourceCode').once('value');
-        sourceCode = snapshot.val();
-        snapshot = await code.child('language').once('value');
-        compiler = snapshot.val();
-        snapshot = await code.child('submissionInput').once('value');
-        submissionInput = snapshot.val();
-        this.SecWidget[ctr].loadSourceCode(compiler,
-          sourceCode,
-          submissionInput);
-          ctr++;
-      }
+      
     }
   }
 }
--- src/app/services/code.service.ts
+++ src/app/services/code.service.ts
@@ -14,8 +14,9 @@ export class CodeService {
   public submitCode(chatID : string,
                     index : string,
                     sourceCode : string,
-                    language : string,
+                    language : number,
                     submissionInput : string) {
+        console.log('chatCodes/' + chatID + "/" + index);
     var ref = firebase.database().ref('chatCodes/' + chatID + "/" + index);
     ref.set({
       sourceCode : sourceCode,
@@ -32,7 +33,7 @@ export class CodeService {
 
   SEC : any;
 
-  public refreshCodes() {
+  public async refreshCodes(SecWidget : any[]) {
     this.SEC = (window as any).SEC || ((window as any).SEC = []);
     var js, fjs = document.getElementsByTagName("script")[0];
 
@@ -44,6 +45,63 @@ export class CodeService {
     let SEC_BASE = "compilers.widgets.sphere-engine.com"; 
     js.src = (SEC_HTTPS ? "https" : "http") + "://" + SEC_BASE + "/static/sdk/sdk.js";
     fjs.parentNode.insertBefore(js, fjs);
+
+    SecWidget[0] = this.SEC.widget("A");
+    SecWidget[0].events.subscribe('beforeSendSubmission', this.submitA.bind(this));
+    SecWidget[1] = this.SEC.widget("B");
+    SecWidget[1].events.subscribe('beforeSendSubmission', this.submitB);
+    SecWidget[2] = this.SEC.widget("C");
+    SecWidget[2].events.subscribe('beforeSendSubmission', this.submitC);
+
+      let widgets = ["A", "B", "C"];
+      let ctr = 0;
+      for (let index of widgets) {
+        var code = this.getCode("X", index);
+
+        let sourceCode : string;
+        let compiler : number;
+        let submissionInput : string;
+        let snapshot = await code.child('sourceCode').once('value');
+        sourceCode = snapshot.val();
+        snapshot = await code.child('language').once('value');
+        compiler = snapshot.val();
+        snapshot = await code.child('submissionInput').once('value');
+        submissionInput = snapshot.val();
+        SecWidget[ctr].loadSourceCode(compiler,
+          sourceCode,
+          submissionInput);
+          ctr++;
+      }
+  }
+
+  public submitA (data : any) {
+    this.submitCode("X",
+                                "A",
+                                data.submissionSource,
+                                data.submissionLanguage,
+                                data.submissionInput);
+    console.log("SUBMITTED");
+    return true;
+  }
+  public submitB = (data) => {
+    this.submitCode("X",
+                                "B",
+                                data.submissionSource,
+                                data.submissionLanguage,
+                                data.submissionInput);
+
+    console.log("SUBMITTED");
+    return true;
+  }
+  public submitC = (data) => {
+    this.submitCode("X",
+                                "C",
+                                data.submissionSource,
+                                data.submissionLanguage,
+                                data.submissionInput);
+
+    console.log("SUBMITTED");
+    return true;
   }
 
   public ready(e : any) : boolean {
